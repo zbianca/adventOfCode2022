af7d749474804aeeb91bbd6fbeba1642fac45de1
Bianca Zanardi <bianca.zanardi@gmail.com>
2022-12-11 16:23:11 +0100

    Refactor day 10

--- day10.js
+++ day10.js
@@ -2,30 +2,29 @@ const {
   datefy
 } = require('./utils');
 
-const data = datefy(10);
+const data = datefy(10, x => x === 'noop' ? null : parseInt(x.split(' ')[1], 10));
 
-function cyclePower(cmds) {
-  let sum = 0;
-  let x = 1;
-  let cycle = 1;
+function saveCycles(cmds) {
+  const cycles = [0, 1];
 
-  const checkCycle = () => {
-    if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
-      sum += x * cycle;
+  for (let i = 0; i < cmds.length; i++) {
+    const cmd = cmds[i];
+    const last = cycles.at(-1);
+    if (cmd === null) cycles.push(last);
+    else {
+      cycles.push(last, last + cmd)
     }
   }
 
-  for (let cmd of cmds) {
-    if (cmd === 'noop') {
-      cycle+= 1;
-      checkCycle();
-    } else {
-      cycle+= 1;
-      checkCycle();
-      cycle+= 1;
-      x+= parseInt(cmd.split(' ')[1], 10);
-      checkCycle();
-    }
+  return cycles;
+}
+
+function cyclePower(cmds) {
+  const cyclesX = saveCycles(cmds);
+  let sum = 0;
+
+  for (let cycle = 20; cycle <= 220; cycle+= 40) {
+    sum += cyclesX[cycle] * cycle;
   }
 
   return sum;
@@ -33,38 +32,13 @@ function cyclePower(cmds) {
 
 console.log(cyclePower(data)); // -> 13480
 
-function saveCycles(cmds) {
-  let x = 1;
-  let cycle = 1;
-
-  const cycles = {
-    1: 1,
-  }
-
-  for (let cmd of cmds) {
-    if (cmd === 'noop') {
-      cycle+= 1;
-      cycles[cycle] = x;
-    } else {
-      cycle+= 1;
-      cycles[cycle] = x;
-      cycle+= 1;
-      x+= parseInt(cmd.split(' ')[1], 10);
-      cycles[cycle] = x;
-    }
-  }
-
-  return cycles;
-
-}
 function paintCycles(cmds) {
   const cyclesX = saveCycles(cmds);
   const result = Array.from({length: 6}, () => new Array(40).fill(' '));
 
   for (let y = 0; y < 240; y++) {
     const curX = cyclesX[y + 1];
-    const observe = [curX - 1, curX, curX + 1];
-    if (observe.includes(y % 40)) {
+    if ([curX - 1, curX, curX + 1].includes(y % 40)) {
       result[Math.trunc(y / 40)][y % 40] = '#';
     }
   }
