8c12bffba1bb7bc1175eebf16464152b540911f5
Bianca Zanardi <bianca.zanardi@gmail.com>
2022-12-11 18:35:21 +0100

    Format day 10

--- day10.js
+++ day10.js
@@ -1,8 +1,8 @@
-const {
-  datefy
-} = require('./utils');
+const { datefy } = require("./utils");
 
-const data = datefy(10, x => x === 'noop' ? null : parseInt(x.split(' ')[1], 10));
+const data = datefy(10, (x) =>
+  x === "noop" ? null : parseInt(x.split(" ")[1], 10)
+);
 
 function saveCycles(cmds) {
   const cycles = [0, 1];
@@ -11,9 +11,7 @@ function saveCycles(cmds) {
     const cmd = cmds[i];
     const last = cycles.at(-1);
     if (cmd === null) cycles.push(last);
-    else {
-      cycles.push(last, last + cmd)
-    }
+    else cycles.push(last, last + cmd);
   }
 
   return cycles;
@@ -23,7 +21,7 @@ function cyclePower(cmds) {
   const cyclesX = saveCycles(cmds);
   let sum = 0;
 
-  for (let cycle = 20; cycle <= 220; cycle+= 40) {
+  for (let cycle = 20; cycle <= 220; cycle += 40) {
     sum += cyclesX[cycle] * cycle;
   }
 
@@ -34,17 +32,17 @@ console.log(cyclePower(data)); // -> 13480
 
 function paintCycles(cmds) {
   const cyclesX = saveCycles(cmds);
-  const result = Array.from({length: 6}, () => new Array(40).fill(' '));
+  const result = Array.from({ length: 6 }, () => new Array(40).fill(" "));
 
   for (let y = 0; y < 240; y++) {
     const curX = cyclesX[y + 1];
     if ([curX - 1, curX, curX + 1].includes(y % 40)) {
-      result[Math.trunc(y / 40)][y % 40] = '#';
+      result[Math.trunc(y / 40)][y % 40] = "#";
     }
   }
 
   for (line of result) {
-    console.log(line.join(''));
+    console.log(line.join(""));
   }
 }
 
